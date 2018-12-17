e87d5d0fcc960661fd7c33c121dfb580e0e13d01
charles32110 <596147986@qq.com>
2018-12-17 17:54:50 +1300

    第二小节——第一部分

--- studyday3.py
+++ studyday3.py
@@ -1,1 +1,10 @@
 #coding = 'utf-8'
+"""
+介绍urllib和urlilb2
+首先安装python对应库，
+pip install urllib
+pip install urllib2
+然后自信学习，打开console 默默输入help查看相关用法，这里文本过多，附上相应官方文档：
+ https://docs.python.org/library/urllib
+ https://docs.python.org/library/urllib2
+"""
